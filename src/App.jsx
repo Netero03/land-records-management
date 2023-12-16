3c039106add88fbe76846bf064e470bc5871bf81
your <jaiswalanshikaajay.7@gmail.com>
2023-12-16 16:48:24 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/Home'
+import MainPage from './MainPage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       <div>
-        <Home/>
-       </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home/>}>
+        </Route>
+        <Route path='/*' element={<MainPage/>}>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
