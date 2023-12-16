34017eadc141ba0761657c5cb486a7fbd70a57a7
your <jaiswalanshikaajay.7@gmail.com>
2023-12-16 15:58:06 +0530

    tailwind

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className=' h-screen w-full flex items-center mx-10'>
+      Land Registration Using Blockchain
+    </div>
+  )
+}
+
+export default Home
