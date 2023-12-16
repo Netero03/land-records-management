886585793a462629919b98099ece76f7107bd9c1
Netero03 <jatinletsgo@gmail.com>
2023-12-16 16:51:18 +0530

    shadow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,35 +2,35 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="bg-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+    <nav className="bg-white flex justify-between items-center h-20 max-w-full mx-auto px-4 text-white border-b-1 border-black shadow-lg">
       {/* Logo */}
       <h1
  
-className="w-full text-3xl font-bold text-[#00df9a]">LAND REGISTRY</h1>
+className="w-4/12 text-xl px-10 font-bold text-[#000000]">LAND REGISTRY</h1>
       {/* Links */}
-      <ul className="flex space-x-4">
+      <ul className="flex  justify-around w-4/12">
         <li>
-          <a href="#" className="text-base font-medium hover:text-gray-300">
+          <a href="#" className="text-base text-gray-600 font-normal hover:text-black">
             Home
           </a>
         </li>
-        <li>
-          <a href="#" className="text-base font-medium hover:text-gray-300">
+        <li> 
+          <a href="#" className="text-base text-gray-600 font-normal hover:text-black">
             User
           </a>
         </li>
         <li>
-          <a href="#" className="text-base font-medium hover:text-gray-300">
+          <a href="#" className="text-base text-gray-600 font-normal hover:text-black">
             Land Inspector
           </a>
         </li>
         <li>
-          <a href="#" className="text-base font-medium hover:text-gray-300">
+          <a href="#" className="text-base text-gray-600 font-normal hover:text-black">
             Contract Owner
           </a>
         </li>
         <li>
-          <a href="#" className="text-base font-medium hover:text-gray-300">
+          <a href="#" className="text-base text-gray-600 font-normal hover:text-black">
             About
           </a>
         </li>
